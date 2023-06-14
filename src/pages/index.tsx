da4041527286a14e1b6860bbb4804908461aba61
S1thK3nny <kennethildebrandt@gmail.com>
2023-06-14 14:17:58 +0200

    Fix "Danger" text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -123,10 +123,10 @@ export default function Home() {
 
               <div role="alert" id='alertDiv' className='hidden mt-4'>
                 <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-                  Danger
+                  Misinput
                 </div>
                 <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                  <p>Something not ideal might be happening.</p>
+                  <p>This doesn't seem like a URL...</p>
                 </div>
               </div>
 
